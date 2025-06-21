feb1728fa4f3c23c523f31a32efb71a87df88a8a
Mohammad Jouza <m.jozaa@yahoo.com>
2025-06-21 10:40:04 +0300

    25-06-21

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+/*
+
 https://www.youtube.com/watch?v=-pYZHR53LGc&list=PLXYGtZKaS4MWjElPL5Z-yVh2QAWUxv0dZ&index=1&ab_channel=MERAKIAcademyJO
 
 
@@ -19,3 +21,37 @@ PLXYGtZKaS4MWjElPL5Z-yVh2QAWUxv0dZ
 
 
 https://www.youtube.com/playlist?list=PLXYGtZKaS4MWjElPL5Z-yVh2QAWUxv0dZ&index=2
+
+
+wasfyaemad@gmail.com
+وصفية عماد
+0799572021
+wasfyaemad@gmail.com
+https://drive.google.com/open?id=12yGgN71y6K0FokNuU75gjYD8pVhGE3jBeLfTtFSL7hg	
+https://drive.google.com/open?id=1QSbka1FrkaPS4wdz0q3wnO5WEJAt0HYg2cyCjED1HMk	
+https://drive.google.com/open?id=1uhZkj8DmnnN1kbZSJhMBFy0bpkaszhvHwoX-ubUnfWs	
+https://drive.google.com/open?id=1bYULTyAcMj3YbdzvSWA5lgC4ISQ350JC	
+13	
+0788812223
+محمد غزال 	
+https://drive.google.com/open?id=1CKFQQHloA6tXjLXsUWBeauB9PqG28zI1
+
+
+============
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  */
+
+//
